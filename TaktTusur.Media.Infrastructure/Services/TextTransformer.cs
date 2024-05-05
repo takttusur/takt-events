15625ef414cc0010c8b1936360a19186b4431181
Andrei Golubkov <andreygolubkow@gmail.com>
2024-05-05 12:23:42 +0400

    Cut text if length exceed

--- TaktTusur.Media.Infrastructure/Services/TextTransformer.cs
+++ TaktTusur.Media.Infrastructure/Services/TextTransformer.cs
@@ -6,7 +6,22 @@ public class TextTransformer : ITextTransformer
 {
 	public string MakeShorter(string text, int maxLength = 0, int maxParagraphs = 0)
 	{
-		//TODO:
+		if (text.Length < maxLength) return text;
+		
+		if (maxLength > 0 && text.Length > maxLength)
+		{
+			var lastSpaceIndex = text.LastIndexOf(' ', maxLength);
+
+			text = text.Substring(0, lastSpaceIndex > 0 ? lastSpaceIndex : maxLength);
+		}
+
+		if (maxParagraphs <= 0) return text;
+		var paragraphs = text.Split(new[] { "\r\n\r\n", "\n\n" }, StringSplitOptions.RemoveEmptyEntries);
+		if (paragraphs.Length > maxParagraphs)
+		{
+			text = string.Join("\r\n\r\n", paragraphs.Take(maxParagraphs));
+		}
+
 		return text;
 	}
 }
